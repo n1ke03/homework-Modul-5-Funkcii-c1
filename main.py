1cee82dd7f85f3a4892db535f04bbd94a90c09c8
Andrey Mikhailov <andreymikhailov03@gmail.com>
2024-04-19 11:59:24 +0300

    1 и 2 задание

---
+++ main.py
@@ -0,0 +1,64 @@
+# Задание 1
+# Напишите функцию, которая отображает на экран
+# форматированный текст, указанный ниже:
+# “Don't compare yourself with anyone in this world…
+# if you do so, you are insulting yourself.”
+# Bill Gates
+
+# def text():
+#     return print(f"“Don't compare yourself with anyone in this world…\nif you do so, you are insulting yourself.”\nBill Gates")
+# text()
+
+
+# Задание 2
+# Напишите функцию, которая принимает два числа
+# в качестве параметра и отображает все четные числа
+# между ними.
+
+# a = int(input())
+# b = int(input())
+# def num():
+#     for i in range(a,b):
+#       if i%2==0:
+#          print(i)
+# num()
+
+
+# Задание 3
+# Напишите функцию, которая отображает пустой или
+# заполненный квадрат из некоторого символа. Функция
+# принимает в качестве параметров: длину стороны ква-
+# драта, символ и переменную логического типа:
+# ■ если она равна True, квадрат заполненный;
+# ■ если False, квадрат пустой.
+
+# def printSquere(lenght:int, simbol:str, fill:bool)->None:
+#     l = lenght+2
+#     s = ''
+#     for x in range(l):
+#       for y in range(l):
+#         if x==0 or x==l-1:
+#           if y==0:
+#             s+= ' -'
+#           elif y==l-1:
+#             s+= ''
+#           else:
+#             s+= '--'
+#         else:
+#           if y==0 or y==l-1:
+#             if y==0:  
+#               s+= '|'
+#             if y==l-1:
+#               s+= ' |'
+#           else:
+#             if fill:        
+#               s+= ' '+simbol
+#             else:
+#               s+= '  '
+#       s+='\n'
+#     print(s)
+  
+# l = int(input('Введите сторону квадрата'))
+# s = input('Введите символ')
+# z = bool(int(input('Закрашивать? (0,1)')))
+# printSquere(l, s, z)  
